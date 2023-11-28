b8f9a89eb955278786ae4a0cbd443ce0cf0d5e9c
Han Sena <abisena@outlook.com>
2023-11-28 12:02:40 +0700

    Anagram

---
+++ Playground/Anagram.playground/Contents.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+func isAnagram(_ s: String, _ t: String) -> Bool {
+    let lowercaseS = s.lowercased()
+    let lowercaseT = t.lowercased()
+    
+    let cortageOfS = lowercaseS.map { ($0, 1)}
+    let cortageOfT = lowercaseT.map { ($0, 1)}
+    
+    let setOfS = Dictionary(cortageOfS) { key, char in key + 1 }
+    let setOfT = Dictionary(cortageOfT) { key, char in key + 1 }
+    return setOfS == setOfT
+}
+
+isAnagram("Hello world", "word helllo")
+
+// shorter version
+func isAnagramShorterVersion(_ s: String, _ t: String) -> Bool {
+    let lowercaseS = s.lowercased()
+    let lowercaseT = t.lowercased()
+    
+    return Dictionary(grouping: lowercaseS, by: { $0 })
+        .mapValues { $0.count } == Dictionary(grouping: lowercaseT, by: { $0 }).mapValues { $0.count }
+}
+
+print(isAnagramShorterVersion("Hello world", "word helllo"))
+
