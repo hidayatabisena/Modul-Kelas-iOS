ac347db1aefef8c6f94700638a61f9725e782096
Han Sena <abisena@outlook.com>
2023-11-28 12:26:23 +0700

    Make the func even shorter

--- Playground/Anagram.playground/Contents.swift
+++ Playground/Anagram.playground/Contents.swift
@@ -12,16 +12,19 @@ func isAnagram(_ s: String, _ t: String) -> Bool {
     return setOfS == setOfT
 }
 
-isAnagram("Hello world", "word helllo")
+isAnagram("Supermassive Black Hole", "Bulkier Chasmovers Ale")
 
 // shorter version
 func isAnagramShorterVersion(_ s: String, _ t: String) -> Bool {
     let lowercaseS = s.lowercased()
     let lowercaseT = t.lowercased()
     
-    return Dictionary(grouping: lowercaseS, by: { $0 })
-        .mapValues { $0.count } == Dictionary(grouping: lowercaseT, by: { $0 }).mapValues { $0.count }
+    let leftS = Dictionary(grouping: lowercaseS, by: { $0 })
+        .mapValues { $0.count }
+    let rightT = Dictionary(grouping: lowercaseT, by: { $0 }).mapValues { $0.count }
+    
+    return leftS == rightT
 }
 
-print(isAnagramShorterVersion("Hello world", "word helllo"))
+print(isAnagramShorterVersion("Supermassive Black Hole", "Bulkier Chassmoves Pale"))
 
