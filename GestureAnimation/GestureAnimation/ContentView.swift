4a961418565550c7522f8dd8cc65902b91fff089
Han Sena <abisena@outlook.com>
2023-12-22 16:07:51 +0700

    Flip degress animation

--- GestureAnimation/GestureAnimation/ContentView.swift
+++ GestureAnimation/GestureAnimation/ContentView.swift
@@ -10,6 +10,9 @@ import SwiftUI
 struct ContentView: View {
     @State private var isImagePressed = false
     @State private var imageToShow = "iOS-Dev-Boy"
+    @State private var dragOffset = CGSize.zero
+    @State private var imageOpacity = 0.0
+    @State private var flipDegrees = 0.0
     
     var body: some View {
         Image(imageToShow)
@@ -20,11 +23,31 @@ struct ContentView: View {
                     self.isImagePressed = false
                 }
             }
+            .opacity(imageOpacity)
+            .rotation3DEffect(
+                .degrees(flipDegrees),
+                axis: (x: 0, y: 1, z: 0)
+            )
+            .offset(dragOffset)
+            .gesture(
+                DragGesture()
+                    .onChanged { gesture in
+                        self.dragOffset = gesture.translation
+                    }
+                    .onEnded { _ in
+                        self.dragOffset = .zero
+                    }
+            )
             .gesture(
                 TapGesture().onEnded { _ in
-                    handleTapGesture()
+                    handleFlipGesture()
                 }
             )
+            .onAppear {
+                withAnimation(.easeIn(duration: 3.0)) {
+                    self.imageOpacity = 1.0
+                }
+            }
     }
     
     private func handleTapGesture() {
@@ -37,6 +60,22 @@ struct ContentView: View {
                 // Ganti image dan reset state
                 self.imageToShow = self.imageToShow == "iOS-Dev-Boy" ? "iOS-Dev-Female" : "iOS-Dev-Boy"
                 self.isImagePressed = false
+            }
+        }
+    }
+    
+    private func handleFlipGesture() {
+        withAnimation(.easeInOut(duration: 0.5)) {
+            // Update degree untuk flip
+            flipDegrees += 180
+            // Ganti image setelah setengah flip
+            // if flipDegrees.truncatingRemainder(dividingBy: 360) == 0 {
+            //    imageToShow = imageToShow == "iOS-Dev-Boy" ? "iOS-Dev-Female" : "iOS-Dev-Boy"
+            // }
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.25) {
+                withAnimation {
+                    imageToShow = imageToShow == "iOS-Dev-Boy" ? "iOS-Dev-Female" : "iOS-Dev-Boy"
+                }
             }
         }
     }
